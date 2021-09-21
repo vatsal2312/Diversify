f8e288eb6f947a08ed9a3b14561fa823fb68b979
vatsal2312 <83644350+vatsal2312@users.noreply.github.com>
2021-09-21 16:39:37 +0530

    Update UpgradableDiversify_V1.sol

--- contracts/token/UpgradableDiversify_V1.sol
+++ contracts/token/UpgradableDiversify_V1.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
-import 'hardhat/console.sol';
+import './console.sol';
 import './ERC20Upgradeable.sol';
 import './OwnableUpgradeable.sol';
 import './Initializable.sol';
